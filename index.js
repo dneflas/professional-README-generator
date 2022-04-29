b65d1c0655a8e6138731b166d97013b59bedbbe9
Debbie Neflas <debbieneflas@gmail.com>
2022-04-28 23:58:25 -0700

    questions array

--- index.js
+++ index.js
@@ -1,14 +1,101 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquierer');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput){
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: '',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true;
+            } else { 
+                console.log('Please enter a project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples of how to run tests.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license for your project.',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is your GitHub username?",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(readmeData => {
+        console.log(readmeData)
+    });
